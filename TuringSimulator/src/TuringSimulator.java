8ef894037f49b8c75bde13da1feca63a872fd3d1
cs361 <omgwtfdeletemealready@gmail.com>
2014-05-07 20:06:40 -0600

    Minor cleanup and formatting.  Added class comment header.  Basically this is the commit test.

--- TuringSimulator/src/TuringSimulator.java
+++ TuringSimulator/src/TuringSimulator.java
@@ -2,26 +2,32 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+/**
+ * This is the driver class for a Turing Machine Simulator.
+ * 
+ * @author Jason Allen
+ * @author Nathan Henninger
+ * @author Matt Weaver
+ *
+ */
 public class TuringSimulator {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		if (args.length == 0) {
+		if (args.length != 1) {
 		  System.err.println("Usage: java TuringSimulator filename");
 		  System.exit(0);
 		}
 	  
 		Machine m = new Machine(new File(args[0]));
-	  ArrayList<Integer> tape = m.run();
+		ArrayList<Integer> tape = m.run();
 		
 		int score = 0;
-    for (int i = 0; i < tape.size(); i++) {
-      int cellContents = tape.get(i);
-      System.out.print(cellContents);
-      score += cellContents;
-    }
-    System.out.println();
-		
-		System.out.println("Score: " + score);
+		for (int i = 0; i < tape.size(); i++) {
+			int cellContents = tape.get(i);
+			System.out.print(cellContents);
+			score += cellContents;
+		}
+    System.out.println("\nScore: " + score);
 	}
 
 }
